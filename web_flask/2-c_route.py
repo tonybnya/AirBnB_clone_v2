d02f8bdec62ebeef11cde8ead26fb4c543d52746
Tony <nya.tony2010@gmail.com>
2023-05-03 20:08:03 +0100

    Add: 2-c_route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -20,5 +20,11 @@ def hbnb():
     return 'HBNB'
 
 
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    '''Displays "C" followed by the value of the text variable'''
+    return f'C {text.replace("_", " ")}'
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
